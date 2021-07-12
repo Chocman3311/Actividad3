763b3b71d4e10b5c25afaee81fbe908ef421608b
Chocman3311 <seba.molinar@duocuc.cl>
2021-07-12 16:51:46 -0400

    Urls y vistas
    Se agregan las urls y las vistas

--- Prueba3/core/views.py
+++ Prueba3/core/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+#PAGINAS
+
+def index(request):
+    
+    return render(request, 'core/index.html')
+
+def trabajos(request):
+
+
+    return render(request, 'core/trabajos.html')
+
+def acercade(request):
+
+    return render(request, 'core/acercade.html')
+
+def formulario(request):
+
+    return render(request, 'core/formulario.html')
+
+def api(request):
+
+    return render(request, 'core/api.html')
+
+#PAGINAS
